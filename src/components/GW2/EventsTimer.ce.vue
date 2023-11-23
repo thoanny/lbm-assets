6c873aa38e0e7c309ba246598e491c7160cdb64c
Anthony Destenay <anthony.destenay@gmail.com>
2023-11-23 10:16:34 +0100

    Fixed time on Events timer

--- src/components/GW2/EventsTimer.ce.vue
+++ src/components/GW2/EventsTimer.ce.vue
@@ -1,26 +1,25 @@
 <script setup>
 import { onMounted, onUnmounted, ref } from 'vue';
-import categories from '../../data/EventsTimer.events.json';
+import eventsTimerData from '../../data/EventsTimer.events.json';
 
 const WIDTH = 10;
 const ONE_MINUTE = 60000;
 
-let rowsTime = {};
 const el = ref();
 const cursorPosition = ref(0);
 const interval = ref(null);
+const categories = ref([]);
 
-function getTheTime(uid, duration) {
-    if (typeof rowsTime[uid] === 'undefined') {
-        rowsTime[uid] = 0;
-    }
+function convertMinutesToText(duration) {
+    const date = new Date();
+    const hours = Math.floor(duration / 60);
+    const minutes = duration % 60;
+    date.setUTCHours(hours, minutes, 0, 0);
+    return `${pad(date.getHours())}:${pad(date.getMinutes())}`;
+}
 
-    const current = rowsTime[uid] - 1440;
-    const hours = Math.floor(current / 60);
-    const minutes = current % 60;
-    rowsTime[uid] += duration;
-
-    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+function pad(d) {
+    return d.toString().padStart(2, '0');
 }
 
 function updatePosition() {
@@ -41,6 +40,23 @@ function resetSize() {
     interval.value = setInterval(updatePosition, ONE_MINUTE);
 }
 
+categories.value = eventsTimerData.map(c => {
+    c.rows.map(r => {
+        let start = 0;
+        r.data = [];
+        r.pattern.forEach((p, i) => {
+            r.data.push({ ...{ 'id': r.uid + '-' + p, 'uid': r.uid + '-' + p + '-' + i }, ...r.events[p], ...{ 'start': start } });
+            start += r.events[p].duration;
+        });
+
+        r.pattern = [];
+        r.events = [];
+
+        return r;
+    })
+    return c;
+});
+
 onMounted(() => {
     updatePosition();
     interval.value = setInterval(updatePosition, ONE_MINUTE);
@@ -53,11 +69,11 @@ onUnmounted(() => {
     interval.value = null;
 });
 
-
 </script>
 
 <template>
-    <div class="lbm-et" ref="el">
+    <!-- <pre>{{ categories }}</pre> -->
+    <div class="lbm-et" ref="el" v-if="categories">
         <div v-for="category in categories" :key="category.uid">
             <div class="lbm-et__category" :style="{ width: WIDTH * 24 * 60 + 'px' }">
                 <span class="sticky left-2">{{ category.name }}</span>
@@ -67,10 +83,10 @@ onUnmounted(() => {
                     <span class="sticky left-2">{{ row.name }}</span>
                 </div>
                 <div class="lbm-et__events">
-                    <div class="lbm-et__events__event" v-for="e, i in row.pattern" :key="row.uid + i"
-                        :style="{ width: row.events[e].duration * WIDTH + 'px', background: row.events[e].background }">
-                        <div v-text="getTheTime(row.uid, row.events[e].duration)"></div>
-                        <div class="truncate font-bold">{{ row.events[e].name }}</div>
+                    <div class="lbm-et__events__event" v-for="e in row.data" :key="e.uid"
+                        :style="{ width: e.duration * WIDTH + 'px', background: e.background }">
+                        <div v-text="convertMinutesToText(e.start)"></div>
+                        <div class="truncate font-bold">{{ e.name }}</div>
                     </div>
                 </div>
             </div>
@@ -106,7 +122,7 @@ $scrollbar: .75rem;
 
 
 .lbm-et {
-    @apply overflow-hidden relative pb-2 leading-4;
+    @apply overflow-x-auto relative pb-2 leading-4 scroll-smooth;
 
 
     &__cursor {
