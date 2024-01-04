bb950cc0d7ab032738923d98ca2c0c65a8ce098c
Anthony Destenay <anthony.destenay@gmail.com>
2024-01-04 14:01:11 +0100

    Fixed fishes v2

--- src/components/GW2/Fishes.ce.vue
+++ src/components/GW2/Fishes.ce.vue
@@ -10,7 +10,7 @@ const token = ref(null);
 
 const allFishes = ref(null);
 const fishes = ref(null);
-const daily = ref({});
+const daily = ref(null);
 
 const achievements = ref([]);
 const holes = ref([]);
@@ -22,22 +22,17 @@ const currentBait = ref('');
 
 const isLoading = ref(false);
 
-const TIMER_INTERVAL = 300000; // 5 minutes
-
-const _times = {
-    'n': 'Nuit',
-    'd': 'Jour',
-    'dd': 'Aube/Crépuscule'
-};
+const TIMER_INTERVAL = 5 * 60 * 1000;
 
 async function getFishes() {
     isLoading.value = true;
-    try {
-        const res = await fetch('https://api.lebusmagique.fr/api/gw2/fishes' + ((apiKey.value) ? '?token=' + apiKey.value : ''));
-        return await res.json();
-    } catch (error) {
-        console.error(error);
-    }
+    const res = await fetch('https://api.lebusmagique.fr/api/gw2/fishes' + ((apiKey.value) ? '?token=' + apiKey.value : ''));
+    return await res.json();
+}
+
+async function getDailyFish() {
+    const res = await fetch('https://api.lebusmagique.fr/api/gw2/fishes/daily');
+    return await res.json();
 }
 
 function updateFishes(reset = false) {
@@ -59,11 +54,11 @@ function updateFishes(reset = false) {
     }
 
     if (currentHole.value) {
-        fishes.value = fishes.value.filter(f => f.hole?.id === currentHole.value);
+        fishes.value = fishes.value.filter(f => f.fishHoles.find(x => x.hole.id === currentHole.value));
     }
 
     if (currentBait.value) {
-        fishes.value = fishes.value.filter(f => f.bait?.uid === currentBait.value);
+        fishes.value = fishes.value.filter(f => f.bait?.item.uid === currentBait.value || f.baitAny === true);
     }
 
     updateFilters();
@@ -79,19 +74,21 @@ function updateFilters() {
 
         achievements.value = sortArrayOfObjects(achievements.value, "name");
 
-        if (f.hole) {
-            const j = holes.value.findIndex(h => h.id === f.hole?.id);
-            if (j < 0) {
-                holes.value.push(f.hole);
-            }
+        if (f.fishHoles) {
+            f.fishHoles.forEach(hole => {
+                const j = holes.value.findIndex(h => h.id === hole.hole.id);
+                if (j < 0) {
+                    holes.value.push(hole.hole);
+                }
+            });
 
             holes.value = sortArrayOfObjects(holes.value, "name");
         }
 
         if (f.bait) {
-            const k = baits.value.findIndex(b => b.uid === f.bait?.uid);
+            const k = baits.value.findIndex(b => b.uid === f.bait.item.uid);
             if (k < 0) {
-                baits.value.push(f.bait);
+                baits.value.push(f.bait.item);
             }
 
             baits.value = sortArrayOfObjects(baits.value, "name");
@@ -137,23 +134,59 @@ function initUserSettings() {
     }
 }
 
-function loadFishes() {
-    getFishes().then(d => {
-        allFishes.value = d.fishes;
-        daily.value = d.daily;
-        token.value = d.token;
-        updateFishes();
-        isLoading.value = false;
+async function loadFishes() {
+    allFishes.value = await getFishes();
+    updateFishes();
+    isLoading.value = false;
+
+    daily.value = await getDailyFish();
+}
+
+function fishHolesToString(holes) {
+    const freq = {
+        'low': 'faible chance',
+        'best': 'meilleure chance',
+        'high': 'fréquemment repéré',
+    };
+    let res = [];
+    holes.forEach((h) => {
+        res.push(h.hole.name + ((h.frequency) ? ' (' + freq[h.frequency] + ')' : ''));
     });
+
+    return res.join(', ');
+}
+
+function fishTimesToString(times) {
+    const freq = {
+        'low': 'faible chance',
+        'best': 'meilleure chance',
+    };
+    let res = [];
+    times.forEach((t) => {
+        res.push(t.time.name + ((t.frequency) ? ' (' + freq[t.frequency] + ')' : ''));
+    });
+
+    return res.join(', ');
 }
 
 initUserSettings();
 
 onMounted(() => {
-    loadFishes();
+    try {
+        loadFishes();
+    } catch (error) {
+        console.error(error);
+    }
+
 
     if (!refreshFishesInterval.value) {
-        refreshFishesInterval.value = setInterval(loadFishes, TIMER_INTERVAL);
+        refreshFishesInterval.value = setInterval(() => {
+            try {
+                loadFishes();
+            } catch (error) {
+                console.error(error);
+            }
+        }, TIMER_INTERVAL);
     }
 });
 
@@ -242,56 +275,57 @@ watch(apiKey, async () => {
             <div class="flex flex-col gap-3 mt-4">
                 <!-- Daily -->
                 <div v-if="daily" class="flex gap-4 items-center p-4 bg-black rounded rounded-lg border border-base-100">
-                    <img :src="'https://api.lebusmagique.fr/uploads/api/gw2/items/' + daily.uid + '.png'"
-                        class="rounded rounded-lg border w-18 h-18 shrink-0" :class="'border-gw2-rarity-' + daily.rarity"
-                        alt="">
+                    <img :src="'https://api.lebusmagique.fr/uploads/api/gw2/items/' + daily.fish.item.uid + '.png'"
+                        class="rounded rounded-lg border w-18 h-18 shrink-0"
+                        :class="'border-gw2-rarity-' + daily.fish.item.rarity" alt="">
                     <div class="flex flex-col gap-1">
                         <div class="inline-flex flex-wrap gap-2 items-center">
-                            <span class="font-bold text-white">{{ daily.name }}</span>
+                            <span class="font-bold text-white">{{ daily.fish.item.name }}</span>
                             <span class="lbm-badge lbm-badge-primary">
                                 Poisson du jour
                             </span>
                         </div>
                         <div class="flex flex-wrap gap-x-2 gap-y-1 text-sm">
-                            <span class="inline-flex gap-1 items-center" v-if="daily.achievement">
+                            <span class="inline-flex gap-1 items-center" v-if="daily.fish.achievement">
                                 <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"
                                     fill="currentColor" class="h-3 w-3">
                                     <path
                                         d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z" />
                                 </svg>
-                                {{ daily.achievement.name }}
+                                {{ daily.fish.achievement.name }}
                             </span>
-                            <span class="inline-flex gap-1 items-center" v-if="daily.hole">
+                            <span class="inline-flex gap-1 items-center" v-if="daily.fish.fishHoles.length > 0">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="h-3 w-3" height="1em"
                                     viewBox="0 0 352 512">
                                     <path
                                         d="M205.22 22.09c-7.94-28.78-49.44-30.12-58.44 0C100.01 179.85 0 222.72 0 333.91 0 432.35 78.72 512 176 512s176-79.65 176-178.09c0-111.75-99.79-153.34-146.78-311.82zM176 448c-61.75 0-112-50.25-112-112 0-8.84 7.16-16 16-16s16 7.16 16 16c0 44.11 35.89 80 80 80 8.84 0 16 7.16 16 16s-7.16 16-16 16z" />
                                 </svg>
-                                {{ daily.hole.name }}
+                                {{ fishHolesToString(daily.fish.fishHoles) }}
                             </span>
-                            <span class="inline-flex gap-1 items-center" v-if="daily.bait">
+                            <span class="inline-flex gap-1 items-center" v-if="daily.fish.bait || daily.fish.baitAny">
                                 <svg xmlns="http://www.w3.org/2000/svg" height="1em" fill="currentColor" class="h-3 w-3"
                                     viewBox="0 0 512 512">
                                     <path
                                         d="M511.988 288.9c-.478 17.43-15.217 31.1-32.653 31.1H424v16c0 21.864-4.882 42.584-13.6 61.145l60.228 60.228c12.496 12.497 12.496 32.758 0 45.255-12.498 12.497-32.759 12.496-45.256 0l-54.736-54.736C345.886 467.965 314.351 480 280 480V236c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v244c-34.351 0-65.886-12.035-90.636-32.108l-54.736 54.736c-12.498 12.497-32.759 12.496-45.256 0-12.496-12.497-12.496-32.758 0-45.255l60.228-60.228C92.882 378.584 88 357.864 88 336v-16H32.666C15.23 320 .491 306.33.013 288.9-.484 270.816 14.028 256 32 256h56v-58.745l-46.628-46.628c-12.496-12.497-12.496-32.758 0-45.255 12.498-12.497 32.758-12.497 45.256 0L141.255 160h229.489l54.627-54.627c12.498-12.497 32.758-12.497 45.256 0 12.496 12.497 12.496 32.758 0 45.255L424 197.255V256h56c17.972 0 32.484 14.816 31.988 32.9zM257 0c-61.856 0-112 50.144-112 112h224C369 50.144 318.856 0 257 0z" />
                                 </svg>
-                                {{ daily.bait.name }}
+                                <span v-if="daily.fish.bait">{{ daily.fish.bait.item.name }}</span>
+                                <span v-if="daily.fish.baitAny">Quelconque</span>
                             </span>
-                            <span class="inline-flex gap-1 items-center" v-if="daily.power">
+                            <span class="inline-flex gap-1 items-center" v-if="daily.fish.power">
                                 <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"
                                     fill="currentColor" class="h-3 w-3">
                                     <path
                                         d="M510.28 445.86l-73.03-292.13c-3.8-15.19-16.44-25.72-30.87-25.72h-60.25c3.57-10.05 5.88-20.72 5.88-32 0-53.02-42.98-96-96-96s-96 42.98-96 96c0 11.28 2.3 21.95 5.88 32h-60.25c-14.43 0-27.08 10.54-30.87 25.72L1.72 445.86C-6.61 479.17 16.38 512 48.03 512h415.95c31.64 0 54.63-32.83 46.3-66.14zM256 128c-17.64 0-32-14.36-32-32s14.36-32 32-32 32 14.36 32 32-14.36 32-32 32z" />
                                 </svg>
-                                {{ daily.power }}
+                                {{ daily.fish.power }}
                             </span>
-                            <span class="inline-flex gap-1 items-center" v-if="daily.time">
+                            <span class="inline-flex gap-1 items-center" v-if="daily.fish.fishTimes.length > 0">
                                 <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"
                                     fill="currentColor" class="h-3 w-3">
                                     <path
                                         d="M256,8C119,8,8,119,8,256S119,504,256,504,504,393,504,256,393,8,256,8Zm92.49,313h0l-20,25a16,16,0,0,1-22.49,2.5h0l-67-49.72a40,40,0,0,1-15-31.23V112a16,16,0,0,1,16-16h32a16,16,0,0,1,16,16V256l58,42.5A16,16,0,0,1,348.49,321Z" />
                                 </svg>
-                                {{ _times[daily.time] }}
+                                {{ fishTimesToString(daily.fish.fishTimes) }}
                             </span>
                         </div>
                     </div>
@@ -319,14 +353,14 @@ watch(apiKey, async () => {
                         <div class="lbm-indicator w-18 h-18 shrink-0">
                             <span class="lbm-indicator-item lbm-indicator-bottom lbm-badge bottom-2 right-2 shadow"
                                 v-if="fish.status" :class="'fish-completed-' + fish.status"></span>
-                            <img :src="'https://api.lebusmagique.fr/uploads/api/gw2/items/' + fish.uid + '.png'"
+                            <img :src="'https://api.lebusmagique.fr/uploads/api/gw2/items/' + fish.item.uid + '.png'"
                                 class="rounded rounded-lg border w-18 h-18 shrink-0"
-                                :class="'border-gw2-rarity-' + fish.rarity" alt="">
+                                :class="'border-gw2-rarity-' + fish.item.rarity" alt="">
                         </div>
 
                         <div>
                             <div class="font-bold text-white">
-                                {{ fish.name }}
+                                {{ fish.item.name }}
                             </div>
                             <div class="flex flex-wrap gap-x-2 gap-y-1 text-sm">
                                 <span class="inline-flex gap-1 items-center" v-if="fish.achievement">
@@ -337,21 +371,22 @@ watch(apiKey, async () => {
                                     </svg>
                                     {{ fish.achievement.name }}
                                 </span>
-                                <span class="inline-flex gap-1 items-center" v-if="fish.hole">
+                                <span class="inline-flex gap-1 items-center" v-if="fish.fishHoles.length > 0">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="h-3 w-3" height="1em"
                                         viewBox="0 0 352 512">
                                         <path
                                             d="M205.22 22.09c-7.94-28.78-49.44-30.12-58.44 0C100.01 179.85 0 222.72 0 333.91 0 432.35 78.72 512 176 512s176-79.65 176-178.09c0-111.75-99.79-153.34-146.78-311.82zM176 448c-61.75 0-112-50.25-112-112 0-8.84 7.16-16 16-16s16 7.16 16 16c0 44.11 35.89 80 80 80 8.84 0 16 7.16 16 16s-7.16 16-16 16z" />
                                     </svg>
-                                    {{ fish.hole.name }}
+                                    <span>{{ fishHolesToString(fish.fishHoles) }}</span>
                                 </span>
-                                <span class="inline-flex gap-1 items-center" v-if="fish.bait">
+                                <span class="inline-flex gap-1 items-center" v-if="fish.bait || fish.baitAny">
                                     <svg xmlns="http://www.w3.org/2000/svg" height="1em" fill="currentColor" class="h-3 w-3"
                                         viewBox="0 0 512 512">
                                         <path
                                             d="M511.988 288.9c-.478 17.43-15.217 31.1-32.653 31.1H424v16c0 21.864-4.882 42.584-13.6 61.145l60.228 60.228c12.496 12.497 12.496 32.758 0 45.255-12.498 12.497-32.759 12.496-45.256 0l-54.736-54.736C345.886 467.965 314.351 480 280 480V236c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v244c-34.351 0-65.886-12.035-90.636-32.108l-54.736 54.736c-12.498 12.497-32.759 12.496-45.256 0-12.496-12.497-12.496-32.758 0-45.255l60.228-60.228C92.882 378.584 88 357.864 88 336v-16H32.666C15.23 320 .491 306.33.013 288.9-.484 270.816 14.028 256 32 256h56v-58.745l-46.628-46.628c-12.496-12.497-12.496-32.758 0-45.255 12.498-12.497 32.758-12.497 45.256 0L141.255 160h229.489l54.627-54.627c12.498-12.497 32.758-12.497 45.256 0 12.496 12.497 12.496 32.758 0 45.255L424 197.255V256h56c17.972 0 32.484 14.816 31.988 32.9zM257 0c-61.856 0-112 50.144-112 112h224C369 50.144 318.856 0 257 0z" />
                                     </svg>
-                                    {{ fish.bait.name }}
+                                    <span v-if="fish.bait">{{ fish.bait.item.name }}</span>
+                                    <span v-if="fish.baitAny">Quelconque</span>
                                 </span>
                                 <span class="inline-flex gap-1 items-center" v-if="fish.power">
                                     <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"
@@ -361,13 +396,13 @@ watch(apiKey, async () => {
                                     </svg>
                                     {{ fish.power }}
                                 </span>
-                                <span class="inline-flex gap-1 items-center" v-if="fish.time">
+                                <span class="inline-flex gap-1 items-center" v-if="fish.fishTimes.length > 0">
                                     <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"
                                         fill="currentColor" class="h-3 w-3">
                                         <path
                                             d="M256,8C119,8,8,119,8,256S119,504,256,504,504,393,504,256,393,8,256,8Zm92.49,313h0l-20,25a16,16,0,0,1-22.49,2.5h0l-67-49.72a40,40,0,0,1-15-31.23V112a16,16,0,0,1,16-16h32a16,16,0,0,1,16,16V256l58,42.5A16,16,0,0,1,348.49,321Z" />
                                     </svg>
-                                    {{ _times[fish.time] }}
+                                    {{ fishTimesToString(fish.fishTimes) }}
                                 </span>
                             </div>
                         </div>
