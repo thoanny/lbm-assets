082f8d9bccca0a8d6dfa8e3b806bfbff56948c28
Anthony Destenay <anthony.destenay@gmail.com>
2024-01-20 18:02:13 +0100

    Renamed Menu to MainMenu

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import './assets/main.scss'
+import './assets/main.scss';
 import '/node_modules/tippy.js/dist/tippy.css';
 
 import { defineCustomElement } from 'vue';
-import LBM_Menu from './components/LBM/Menu.ce.vue';
+import LBM_MainMenu from './components/LBM/MainMenu.ce.vue';
 import GW2_WizardVault from './components/GW2/WizardVault.ce.vue';
 import GW2_EventsTimer from './components/GW2/EventsTimer.ce.vue';
 import GW2_FishingCompanion from './components/GW2/FishingCompanion.ce.vue';
 import GW2_ApiItemTooltip from './components/GW2/ApiItemTooltip.ce.vue';
 
-customElements.define('lbm-menu', defineCustomElement(LBM_Menu));
+customElements.define('lbm-menu', defineCustomElement(LBM_MainMenu));
 customElements.define('gw2-wizard-vault', defineCustomElement(GW2_WizardVault));
 customElements.define('gw2-events-timer', defineCustomElement(GW2_EventsTimer));
 customElements.define('gw2-fishes', defineCustomElement(GW2_FishingCompanion));
