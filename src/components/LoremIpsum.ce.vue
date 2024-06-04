5c13c4f0f1513790ededed7fee221f4207888b32
Anthony Destenay <anthony.destenay@gmail.com>
2024-06-04 09:23:00 +0200

    Added Lorem ipsum

---
+++ src/components/LoremIpsum.ce.vue
@@ -0,0 +1,44 @@
+<template>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget ipsum facilisis, congue
+    tellus id, gravida nisl. Nunc cursus imperdiet est, id fermentum mi congue fringilla. Maecenas
+    condimentum vulputate libero ut efficitur. Integer nunc metus, pharetra sed nisl id, tempus
+    volutpat turpis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+    ridiculus mus. Sed mattis nibh erat, sit amet molestie diam interdum sed. Proin ut sollicitudin
+    ipsum. Nulla ac feugiat quam. Phasellus in rhoncus orci. Vivamus eget vulputate tellus.
+    Suspendisse sit amet semper erat. Quisque vehicula ipsum nec viverra suscipit. Praesent sit amet
+    lorem justo.
+  </p>
+  <p>
+    Praesent felis felis, interdum in dictum et, mattis at dui. Curabitur mollis fringilla erat.
+    Integer congue non arcu nec bibendum. Aenean sed augue dictum, molestie enim ac, consectetur
+    tortor. Donec hendrerit, urna id porta aliquam, diam neque commodo dui, eu condimentum orci
+    ligula in lacus. In in odio enim. Nulla aliquam tincidunt orci, nec condimentum sapien mattis
+    id.
+  </p>
+  <p>
+    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+    Curabitur id ultrices quam. Etiam efficitur justo ultricies nibh volutpat, at rhoncus dolor
+    fringilla. Etiam auctor erat vitae nunc vestibulum, eu malesuada lorem bibendum. In quis iaculis
+    purus, eu dignissim nulla. Pellentesque finibus, purus et blandit hendrerit, dui elit euismod
+    urna, a efficitur nunc metus eu ex. Curabitur ornare ipsum turpis, a lacinia magna tempor ut.
+    Integer vulputate bibendum tellus, non malesuada quam porta pellentesque. Phasellus in accumsan
+    nisi, id imperdiet nisl. Etiam consequat, mauris quis accumsan porttitor, felis massa
+    ullamcorper felis, in lacinia lacus lectus in nibh.
+  </p>
+  <p>
+    Sed quis feugiat ipsum. Fusce ornare sed lacus eu ullamcorper. Suspendisse erat lectus, viverra
+    a odio interdum, imperdiet interdum lorem. Nullam et mi et ipsum rhoncus pharetra vitae nec
+    turpis. Donec vitae fringilla est. Fusce nulla nunc, porttitor at ligula sed, aliquet fringilla
+    augue. Aliquam aliquam sapien nec dui pharetra, vel tempor sapien suscipit.
+  </p>
+  <p>
+    Sed id nisi sit amet nibh fermentum gravida. Pellentesque vel lectus non arcu mollis volutpat
+    varius a dui. Sed dolor ante, scelerisque et purus nec, varius egestas eros. Sed et eleifend mi.
+    Nam aliquam enim a mi blandit aliquam eu semper velit. Ut efficitur nisl eu justo placerat
+    eleifend. Suspendisse fringilla pretium pellentesque. Vestibulum euismod felis eu pharetra
+    posuere. Mauris facilisis, dui in consectetur mattis, sapien sapien auctor turpis, eu luctus
+    dolor elit et felis. Proin id justo leo. Etiam viverra lorem et gravida finibus. Phasellus ut
+    gravida massa, non posuere velit. Praesent mattis tortor et eros bibendum condimentum.
+  </p>
+</template>
