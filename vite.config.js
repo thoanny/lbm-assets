860e593e26cf39d2ba9e149afdd17c76c7b314e6
Anthony Destenay <anthony.destenay@gmail.com>
2024-08-02 11:23:55 +0200

    Fixed base URL

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-// import Spritesmith from 'vite-plugin-spritesmith';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: 'https://lbm-assets.vercel.app',
+  base: 'https://lbm-assets.vercel.app',
   plugins: [
     vue({
       template: {
@@ -15,39 +14,6 @@ export default defineConfig({
         },
       },
     }),
-    // Spritesmith({
-    //   src: {
-    //     cwd: './src/sprites',
-    //     glob: '*.png',
-    //   },
-    //   target: {
-    //     image: './public/sprite.png',
-    //     css: [
-    //       [
-    //         './src/assets/_sprite.scss',
-    //         {
-    //           format: 'handlebars_based_template',
-    //         },
-    //       ],
-    //       [
-    //         './src/data/sprite.json',
-    //         {
-    //           format: 'json_array',
-    //         },
-    //       ],
-    //     ],
-    //   },
-    //   apiOptions: {
-    //     cssImageRef: '/sprite.png',
-    //     spritesheet_info: {
-    //       name: 'spritesmith',
-    //       format: 'handlebars_based_template',
-    //     },
-    //   },
-    //   customTemplates: {
-    //     handlebars_based_template: './src/scss.handlebars',
-    //   },
-    // }),
   ],
   resolve: {
     alias: {
