10edb20be80822352868aa7bffbf65af44401004
Anthony Destenay <anthony.destenay@gmail.com>
2024-09-27 11:42:31 +0200

    Removed base to have a functional preview

--- vite.config.js
+++ vite.config.js
@@ -5,27 +5,27 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://lbm-assets.vercel.app',
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag.includes('-'),
+    // base: 'https://lbm-assets.vercel.app',
+    plugins: [
+        vue({
+            template: {
+                compilerOptions: {
+                    isCustomElement: (tag) => tag.includes('-'),
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: '[name].js',
-        assetFileNames: '[name].[ext]',
-      },
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: '[name].js',
+                assetFileNames: '[name].[ext]',
+            },
+        },
     },
-  },
 });
